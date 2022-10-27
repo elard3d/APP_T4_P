a84adbf1a99176d2f8669414394bec71d76a2aaa
Elard Burga <elard3d@hotmail.com>
2022-10-27 08:35:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/app_t4_p/MostrarAnimesActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_t4_p;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarAnimesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_animes);
+    }
+}
