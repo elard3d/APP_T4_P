e94d114c8f29b39b306015e2a7509be9b5c6719b
Elard Burga <elard3d@hotmail.com>
2022-10-27 14:19:18 -0500

    Button Mapeados

--- app/src/main/java/com/example/app_t4_p/CrearAnimesActivity.java
+++ app/src/main/java/com/example/app_t4_p/CrearAnimesActivity.java
@@ -2,7 +2,10 @@ package com.example.app_t4_p;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CrearAnimesActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class CrearAnimesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_animes);
+
+        Button btnRegistrarAnime = findViewById(R.id.btnRegistrarAnime);
+        Button btnRegresar = findViewById(R.id.btnRegresar);
+
+        btnRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CrearAnimesActivity.this, MainActivity.class );
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/app_t4_p/MainActivity.java
+++ app/src/main/java/com/example/app_t4_p/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.app_t4_p;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnMostrarAnimes = findViewById(R.id.btnMostrarAnimes);
+        Button btnCrearAnimes = findViewById(R.id.btnCrearAnime);
+
+        btnCrearAnimes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CrearAnimesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMostrarAnimes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MostrarAnimesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
